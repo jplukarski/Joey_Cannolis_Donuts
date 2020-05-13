5368074e703269ba49ac25ca506c1861e6386251
Joe Plukarski <joeplukarski@Joes-MacBook-Pro.local>
2020-05-13 16:00:01 -0500

    removed semantic UI due to a bug that still had a PR open and added react bootstrap and also added routing and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Logout from './pages/Logout'
+import Register from './pages/Register'
+import Cart from './pages/Cart'
+import Success from './pages/Success'
+import Failure from './pages/Failure'
+import Navbar from './components/Navbar'
 
-function App(){
+export default function App(){
     return(
-        <>Hello World</>
+        <>
+            <Navbar/>
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route exact path='/login' component={Login}/>
+                    <Route exact path='/logout' component={Logout}/>
+                    <Route exact path='/register' component={Register}/>
+                    <Route exact path='/cart' component={Cart}/>
+                    <Route exact path='/cart/success' component={Success}/>
+                    <Route exact path='/cart/failure' component={Failure}/>
+                </Switch>
+            </Router>
+        </>
     )
 }
-
-export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Link} from 'react-router-dom'
+
+export default function Navbar() {
+    return(
+        <>
+            Hello
+        </>
+    )
+}
