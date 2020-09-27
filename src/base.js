f4f764e0b4e7f36aee7d7ce7dd6b89d396ed5330
Joe Plukarski <joeplukarski@Joes-MacBook-Pro.local>
2020-09-27 15:50:45 -0500

    trying to deploy

--- src/base.js
+++ src/base.js
@@ -3,12 +3,12 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const app = firebase.initializeApp({
-    apiKey: process.env.FIREBASE_KEY,
-    authDomain: process.env.FIREBASE_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_SENDER_ID
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
 });
 
 export default app;
